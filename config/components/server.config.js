7da2d78f6e16517e06bd6d5df1f85d0f959d9bb7
Nathaniel Heitsch <me@nathanielheitsch.dev>
2020-07-05 21:58:18 -0500

    Edit config files

--- config/components/server.config.js
+++ config/components/server.config.js
@@ -1,10 +1,10 @@
 const config = {
-  env: envVars.NODE_ENV,
-  isTest: envVars.NODE_ENV === 'test',
-  isDevelopment: envVars.NODE_ENV === 'development',
+  env: process.env.NODE_ENV,
+  isTest: process.env.NODE_ENV === 'test',
+  isDevelopment: process.env.NODE_ENV === 'development',
   server: {
-    port: envVars.PORT || 3000,
-    apiVersion: `v${envVars.API_VERSION}` || 'v1',
+    port: process.env.PORT || 3000,
+    apiVersion: `v${process.env.API_VERSION}` || 'v1',
   },
 };
 
