139ca7588111204e0636e2477b1720f58b579e6f
Nathaniel Heitsch <me@nathanielheitsch.dev>
2020-07-05 21:33:44 -0500

    Updated package versions

--- config/components/database.config.js
+++ config/components/database.config.js
@@ -1,30 +1,3 @@
-const joi = require('joi');
-
-/**
- * Generate a validation schema using joi to check the type of your environment variables
- */
-const envSchema = joi
-  .object({
-    DB_USER: joi.string(),
-    DB_HOST: joi.string(),
-    DB_PASSWORD: joi
-      .string()
-      .optional()
-      .empty(''),
-    DB_DATABASE: joi.string(),
-    DB_PORT: joi.number(),
-  })
-  .unknown()
-  .required();
-
-/**
- * Validate the env variables using joi.validate()
- */
-const { error, value: envVars } = joi.validate(process.env, envSchema);
-if (error) {
-  throw new Error(`Config validation error: ${error.message}`);
-}
-
 const config = {
   databaseConfig: {
     user: envVars.DB_USER,
--- knexfile.js
+++ knexfile.js
@@ -65,5 +65,25 @@ module.exports = {
     },
   },
 
-  production: {},
+  production: {
+    client: 'postgresql',
+    connection: {
+      port: process.env.DB_PORT,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: process.env.DATABASE_POOL_MIN,
+      max: process.env.DATABASE_POOL_MAX,
+    },
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'knex_migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
+  }
 };
